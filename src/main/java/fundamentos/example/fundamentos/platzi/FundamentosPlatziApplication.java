480ff2cc25c3642de94319114533b3216a296cce
Tiziana Amicarella <tiziana.amicarella@unet.edu.ve>
2023-03-12 22:41:55 -0400

    first commit

---
+++ src/main/java/fundamentos/example/fundamentos/platzi/FundamentosPlatziApplication.java
@@ -0,0 +1,13 @@
+package fundamentos.example.fundamentos.platzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosPlatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosPlatziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fundamentos/example/fundamentos/platzi/FundamentosPlatziApplicationTests.java
@@ -0,0 +1,13 @@
+package fundamentos.example.fundamentos.platzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosPlatziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
